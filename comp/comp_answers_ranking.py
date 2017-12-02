9c951616f748be6474e9821646a7b139bd1a735c
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2017-12-02 14:53:35 +0100

    Fix competitions

--- comp/comp_answers_ranking.py
+++ comp/comp_answers_ranking.py
@@ -5,61 +5,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import savefig
 
-def plotting(external_names, external_scores, contest_number):
-    y_pos = np.arange(len(external_names))
 
-    external_scores = list(map(lambda x: max(0, x), external_scores))
+plot_filename = 'plot.png'
 
-    performance = external_scores
-    plt.bar(y_pos, performance, align='center')
-    plt.xticks(y_pos, external_names)
+
+def plotting(contest_number, highscores):
+    y_pos = range(len(highscores))
+
+    plt.bar(y_pos, [v for _, v in highscores], align='center')
+    plt.xticks(y_pos, [k for (k, _) in highscores])
+    plt.yticks(y_pos)
+
     plt.xlabel('Imię i nazwisko')
     plt.ylabel('Punkty')
     plt.title('Wyniki {} konkursu'.format(contest_number))
 
-    savefig('plot.png', bbox_inches = 'tight')
+    savefig(plot_filename, bbox_inches = 'tight')
+
+
+def contest_exists():
+    return True
+
+
+def get_tasks_per_user(answers_rank):
+    tasks_per_user = {}
+    for e in answers_rank:
+        each = json.loads(e.decode())
+        if not each['user_id'] in tasks_per_user:
+            tasks_per_user[each['user_id']] = set([each['task_id']])
+        else:
+            tasks_per_user[each['user_id']].add(each['task_id'])
+    return tasks_per_user
+
 
 def answers_ranking_parse(redis_client, bot, update):
     assert len(update.message.text.split()) > 1
     assert update.message.text.split()[1].isdigit()
-    result = []
-    temp = []
-    external_scores = []
-    external_names = []
 
-    for each in answers_ranking(redis_client, update.message.text.split()[1]):
-        each = each.decode()
-        each = json.loads(each)
-        contest_number = update.message.text.split()[1]
-        print(each)
-        tid = each['task_id']
-        uid = each['user_id']
-        pair = [uid, tid]
-        print(pair)
-        if pair not in temp:
-            temp.append(pair)
-    sorted(temp)
-    count = 1
-    size = len(temp)
-    if size < 1:
+    contest_id = int(update.message.text.split()[1])
+    tasks_per_user = get_tasks_per_user(answers_ranking(redis_client, contest_id))
+    print(tasks_per_user)
+
+    if not contest_exists():
         bot.send_message(chat_id=update.message.chat_id, text="Nie istnieje konkurs o podanym numerze")
         return
-    for i in range(1, size):
-        if temp[i][0] != temp[i - 1][0]:
-            result.append([temp[i - 1][0], count])
-            count = 1
-            continue
-        count = count + 1
-    result.append([temp[size - 1][0], count])
-    sorted(result)
-    size = len(result)
-    for i in range(0, min(size, 5)):
-        show_user = lambda x: '{} {}'.format(x['first_name'], x['last_name'])
-        full_name = show_user(bot.get_chat(result[i][0]))
-        external_names.append(full_name)
-        text = str(i + 1) + ". " + full_name + " SCORE: " + str(result[i][1])
-        external_scores.append(result[i][1])
-        bot.send_message(chat_id=update.message.chat_id, text=text)
 
-    plotting(external_names, external_scores, contest_number)
-    bot.sendPhoto(chat_id=update.message.chat_id, photo=open('plot.png', 'rb'))
+    show_user = lambda x: '{} {}'.format(x['first_name'], x['last_name'])
+    highscores = sorted([(show_user(bot.get_chat(id)), len(v))
+                for id, v in tasks_per_user.items()], key=lambda x: -x[1])[:5]
+
+    highscores_text = '\n'.join(["{}, score: {}".format(id, score)
+                              for (id, score) in highscores])
+    bot.send_message(chat_id=update.message.chat_id, text=highscores_text)
+
+    plotting(contest_id, highscores)
+    bot.sendPhoto(chat_id=update.message.chat_id, photo=open(plot_filename, 'rb'))
